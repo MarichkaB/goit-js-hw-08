4dccb60f767eee746cd4da3a932fb64a806250fd
Maria Buznik <masha.vinogradova82@gmail.com>
2022-08-10 15:56:35 +0300

    add 1,2,3 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgGallery = document.querySelector('.gallery');
+const imgMarkup = galleryItems
+  .map(
+    item => `<div><a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></div>`
+  )
+  .join('');
+
+imgGallery.insertAdjacentHTML('beforeend', imgMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  captionType: 'alt',
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const input = form.querySelector('input');
+const textarea = form.querySelector('textarea');
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+    const inputForm = {
+      input: form.elements.email.value,
+      textarea: form.elements.message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(inputForm));
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  localStorage.clear();
+});
+
+const storage = localStorage.getItem('feedback-form-state');
+const parseStorage = JSON.parse(storage);
+
+const fillForm = () => {
+  if (parseStorage !== null) {
+    input.value = parseStorage.email;
+    textarea.value = parseStorage.message;
+  }
+};
+fillForm();
